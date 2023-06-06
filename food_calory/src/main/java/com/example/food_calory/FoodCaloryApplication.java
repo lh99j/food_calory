614c966efed266022f6714cc2178bddde92b1838
lh99j <grand2181@gmail.com>
2023-06-06 11:06:44 +0900

    spring boot project setting

---
+++ food_calory/src/main/java/com/example/food_calory/FoodCaloryApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_calory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCaloryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodCaloryApplication.class, args);
+    }
+
+}
---
+++ food_calory/src/test/java/com/example/food_calory/FoodCaloryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_calory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCaloryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
